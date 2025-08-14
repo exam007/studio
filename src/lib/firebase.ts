3acb9abad764e59db96dbca7f950b20230ebb683
Narongtorn S S <narongtorn.s@attorney285.co.th>
2025-08-14 07:19:57 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "examplify-8rgrq",
+  "appId": "1:1056424535307:web:20ebf76ed80c289a1a6e0e",
+  "storageBucket": "examplify-8rgrq.firebasestorage.app",
+  "apiKey": "AIzaSyA5pchbjaCkKzHTMVozvScqKCDlIPdq81s",
+  "authDomain": "examplify-8rgrq.firebaseapp.com",
+  "messagingSenderId": "1056424535307"
 };
 
 // Initialize Firebase
